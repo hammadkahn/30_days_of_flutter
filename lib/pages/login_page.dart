bf5e7a0dd9c7b6d7700536d0a15ce423be32414e
hammadkahn <hammad.khan0312@gmail.com>
2021-12-04 17:00:31 +0500

    day 3 commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class Login_page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text(
+          "LOGIN PAGE",
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
